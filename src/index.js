b0c62103c93dbd6d21f7d5dcbfbcc7d94dbc80c9
yshwang <hysgold@etevers.com>
2023-10-04 11:40:25 +0900

    데이터테이블 visualization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import DataTable from './js/DataTable';
-// import DataFetch from './js/DataFetch.js';
+import DataTable from './js/DataTable';
+import DataFetch from './js/DataFetch.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <DataTable /> */}
-    {/* <DataFetch /> */}
+    <DataTable />
+    <DataFetch />
   </React.StrictMode>
 );
